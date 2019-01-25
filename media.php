8d57869d87f55d46be9dc9dcff7d359aa9d0ba80
AkashBhave <akashbbhave@gmail.com>
2019-01-25 14:52:27 -0500

    Reformat documents section in media.php

--- media.php
+++ media.php
@@ -14,6 +14,11 @@
     <?php require("inc/header.php"); ?>
     
 	<section class="media-container container py-4">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Images and Video</h1>
+            </div>
+        </div>
         <div class="row py-2">
             <div class="col-md-4">
                 <img class="media-image img-fluid img-thumbnail" src="assets/img/DesignProcess/Design.PNG">
@@ -38,21 +43,24 @@
         </div>
     </section>
     
-    <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-        <div class="mdl-card mdl-cell mdl-cell--12-col">
-            <div class="mdl-card__supporting-text">
-
-                <h4>Important Documents</h4>
-                <ul class="bigUL">
-                    <li> The Merit Review, which contains the outline and specifications of TJREVERB, can be found
-                        <a href="assets/pdf/merit-review.pdf" rel="nofollow" target="_blank">here</a>. </li>
-                    <li>The Feasibility Review, which outlines the logistics and overall feasibility of the TJREVERB project, can
-                        be found
-                        <a href="assets/pdf/feasibility-review.pdf" rel="nofollow" target="_blank">here</a>. </li>
-                    <li> The APRS Radio Guide which details the process of setting up the APRS radio, can be found
-                        <a href="https://docs.google.com/document/d/1EdsT4j9h4Mfo8XkUpSk1f3b30yDYg-lx9Cxqp7mObMY/edit?usp=sharing">here</a>.
-                    </li>
-                </ul>
+    <section class="media-container container py-4">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Documents</h1>
+            </div>
+        </div>
+        <div class="row py-2">
+            <div class="col-md-4">
+                The Merit Review, which contains the outline and specifications of TJREVERB, can be found
+                <a href="assets/pdf/merit-review.pdf" rel="nofollow" target="_blank">here</a>. 
+            </div>
+            <div class="col-md-4">
+                The Feasibility Review, which outlines the logistics and overall feasibility of the TJREVERB project, can be found
+                <a href="assets/pdf/feasibility-review.pdf" rel="nofollow" target="_blank">here</a>. 
+            </div>
+            <div class="col-md-4">
+                The APRS Radio Guide which details the process of setting up the APRS radio, can be found
+                <a href="https://docs.google.com/document/d/1EdsT4j9h4Mfo8XkUpSk1f3b30yDYg-lx9Cxqp7mObMY/edit?usp=sharing">here</a>.
             </div>
         </div>
     </section>
